f956594e5a82560b8f9a2dafad183e3471c27b33
lyubomiriliev <iliev.lyubomir98@gmail.com>
2024-03-16 02:03:34 +0200

    Fetching all 4 charging stations params

--- client/src/pages/Stations.jsx
+++ client/src/pages/Stations.jsx
@@ -1,47 +1,76 @@
 import { useEffect, useState } from "react"
 import { chargingStationSVG } from "../assets"
-import { fetchData } from "../utils/api"
+import { fetchDataByala1, fetchDataByala2, fetchDataPrimorsko1, fetchDataPrimorsko2 } from "../utils/api"
+
+
+
 
 
 const Stations = () => {
 
-    const [statusData, setStatusData] = useState(null);
+    const [stationByala1, setStationByala1] = useState(null);
+    const [stationByala2, setStationByala2] = useState(null);
+    const [stationPrimorsko1, setStationPrimorsko1] = useState(null);
+    const [stationPrimorsko2, setStationPrimorsko2] = useState(null);
 
     useEffect(() => {
-        const fetchDataForStation = async () => {
-            const data = await fetchData('vesso@raytex-bg.com', 'tgrnc02YmExVtRiXIjzMpp10D44y2Hyc', '3736');
-            setStatusData(data);
+        const fetchDataForStations = async () => {
+            const dataByala1 = await fetchDataByala1('vesso@raytex-bg.com', 'tgrnc02YmExVtRiXIjzMpp10D44y2Hyc', '3736');
+            const dataByala2 = await fetchDataByala2('vesso@raytex-bg.com', 'tgrnc02YmExVtRiXIjzMpp10D44y2Hyc', '2946');
+            const dataPrimorsko1 = await fetchDataPrimorsko1('vesso@raytex-bg.com', 'tgrnc02YmExVtRiXIjzMpp10D44y2Hyc', '3805');
+            const dataPrimorsko2 = await fetchDataPrimorsko2('vesso@raytex-bg.com', 'tgrnc02YmExVtRiXIjzMpp10D44y2Hyc', '4380');
+            setStationByala1(dataByala1);
+            setStationByala2(dataByala2);
+            setStationPrimorsko1(dataPrimorsko1);
+            setStationPrimorsko2(dataPrimorsko2);
         };
 
-        fetchDataForStation();
+        fetchDataForStations();
     }, []);
 
     return (
         <div className="w-full bg-white py-40 px-4">
-            <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {statusData && (
+            <div className="max-w-screen-xl mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {stationByala1 && (
                     <div className="bg-gray-100 rounded-md p-6 relative flex flex-col justify-center items-center">
                         <img className="w-2/3 h-auto mb-4" src={chargingStationSVG} alt="stationIcon" />
-                        <h2 className="text-xl font-bold mb-2">{statusData.Name}</h2>
-                        <p className="text-xl font-bold text-green-500 mb-2">{statusData.State}</p>
-                        <p className="text-sm text-gray-600 mb-2">22 kwH</p>
-                        <p className="text-sm text-gray-600 absolute bottom-3 right-3">{statusData.code}</p>
+                        <h2 className="text-xl font-bold mb-2">{stationByala1.Name}</h2>
+                        <p className="text-xl font-bold text-green-500 mb-2">{stationByala1.State}</p>
+                        <p className="text-sm text-gray-600 mb-2">Мощност: 22kw</p>
+                        <p className="text-sm text-gray-600 mb-2">Зареден ток: {stationByala1.EVEnergyCharged}kwH</p>
+                        <p className="text-sm text-gray-600 absolute bottom-3 right-3">{stationByala1.code}</p>
                     </div>
                 )}
-                <div className="bg-gray-100 rounded-md p-6 relative flex flex-col justify-center items-center">
-                    <img className="w-2/3 h-auto mb-4" src={chargingStationSVG} alt="stationIcon" />
-                    <h2 className="text-xl font-bold mb-2">Бяла 2</h2>
-                    <p className="text-xl font-bold text-green-500 mb-2">Свободна</p>
-                    <p className="text-sm text-gray-600 mb-2">Мощност: 22kw</p>
-                    <p className="text-sm text-gray-600 absolute bottom-3 right-3">#2946</p>
-                </div>
-                <div className="bg-gray-100 rounded-md p-6 relative flex flex-col justify-center items-center">
-                    <img className="w-2/3 h-auto mb-4" src={chargingStationSVG} alt="stationIcon" />
-                    <h2 className="text-xl font-bold mb-2">Приморско 2</h2>
-                    <p className="text-xl font-bold text-red-500 mb-2">Заета</p>
-                    <p className="text-sm text-gray-600 mb-2">Мощност: 22kw</p>
-                    <p className="text-sm text-gray-600 absolute bottom-3 right-3">#3805</p>
-                </div>
+                {stationByala2 && (
+                    <div className="bg-gray-100 rounded-md p-6 relative flex flex-col justify-center items-center">
+                        <img className="w-2/3 h-auto mb-4" src={chargingStationSVG} alt="stationIcon" />
+                        <h2 className="text-xl font-bold mb-2">{stationByala2.Name}</h2>
+                        <p className="text-xl font-bold text-green-500 mb-2">{stationByala2.State}</p>
+                        <p className="text-sm text-gray-600 mb-2">Мощност: 22kw</p>
+                        <p className="text-sm text-gray-600 mb-2">Зареден ток: {stationByala2.EVEnergyCharged}kwH</p>
+                        <p className="text-sm text-gray-600 absolute bottom-3 right-3">{stationByala2.code}</p>
+                    </div>
+                )}
+                {stationPrimorsko1 && (
+                    <div className="bg-gray-100 rounded-md p-6 relative flex flex-col justify-center items-center">
+                        <img className="w-2/3 h-auto mb-4" src={chargingStationSVG} alt="stationIcon" />
+                        <h2 className="text-xl font-bold mb-2">{stationPrimorsko1.Name}</h2>
+                        <p className="text-xl font-bold text-green-500 mb-2">{stationPrimorsko1.State}</p>
+                        <p className="text-sm text-gray-600 mb-2">Мощност: 22kw</p>
+                        <p className="text-sm text-gray-600 mb-2">Зареден ток: {stationPrimorsko1.EVEnergyCharged}kwH</p>
+                        <p className="text-sm text-gray-600 absolute bottom-3 right-3">{stationPrimorsko1.code}</p>
+                    </div>
+                )}
+                {stationPrimorsko2 && (
+                    <div className="bg-gray-100 rounded-md p-6 relative flex flex-col justify-center items-center">
+                        <img className="w-2/3 h-auto mb-4" src={chargingStationSVG} alt="stationIcon" />
+                        <h2 className="text-xl font-bold mb-2">{stationPrimorsko2.Name}</h2>
+                        <p className="text-xl font-bold text-green-500 mb-2">{stationPrimorsko2.State}</p>
+                        <p className="text-sm text-gray-600 mb-2">Мощност: 22kw</p>
+                        <p className="text-sm text-gray-600 mb-2">Зареден ток: {stationPrimorsko2.EVEnergyCharged}kwH</p>
+                        <p className="text-sm text-gray-600 absolute bottom-3 right-3">{stationPrimorsko2.code}</p>
+                    </div>
+                )}
             </div>
         </div>
     );
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 const URL = "https://tok-bg.com/API/get.php?t=status";
 
-export const fetchData = async (user, token, code) => {
+export const fetchDataByala1 = async (user, token, code) => {
   const response = await fetch(URL, {
     method: "POST",
     headers: {
@@ -10,6 +10,60 @@ export const fetchData = async (user, token, code) => {
       user: "vesso@raytex-bg.com",
       token: "tgrnc02YmExVtRiXIjzMpp10D44y2Hyc",
       code: "3736",
+    }),
+  });
+
+  const clonedResponse = response.clone();
+
+  return clonedResponse.json();
+};
+
+export const fetchDataByala2 = async (user, token, code) => {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      user: "vesso@raytex-bg.com",
+      token: "tgrnc02YmExVtRiXIjzMpp10D44y2Hyc",
+      code: "2946",
+    }),
+  });
+
+  const clonedResponse = response.clone();
+
+  return clonedResponse.json();
+};
+
+export const fetchDataPrimorsko1 = async (user, token, code) => {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      user: "vesso@raytex-bg.com",
+      token: "tgrnc02YmExVtRiXIjzMpp10D44y2Hyc",
+      code: "3805",
+    }),
+  });
+
+  const clonedResponse = response.clone();
+
+  return clonedResponse.json();
+};
+
+export const fetchDataPrimorsko2 = async (user, token, code) => {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      user: "vesso@raytex-bg.com",
+      token: "tgrnc02YmExVtRiXIjzMpp10D44y2Hyc",
+      code: "4380",
     }),
   });
 
